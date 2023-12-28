0ffc1aae1f1284beab89a7aac3912d49e962aac4
zenghuasheng <zenghuasheng@ones.ai>
2023-12-28 18:47:27 +0800

    add: install; 本地重启

--- remote_debug/build_deploy.py
+++ remote_debug/build_deploy.py
@@ -28,11 +28,17 @@ def comment_code():
             lines = file.readlines()
 
         with open('app/init.go', 'w') as file:
+            flag = 0
             for line in lines:
                 if 'github.com/bangwork/ones-anti-piracy/pkg/apis/runtime' in line:
                     file.write('// ' + line)
                 elif 'err := runtime.RegisterInstanceRuntime(db.RedisPool(), "")' in line:
                     file.write('// ' + line)
+                    # 接下来的 3 行也注释掉，先标记
+                    flag = 1
+                elif 0 < flag < 4:
+                    file.write('// ' + line)
+                    flag += 1
                 else:
                     file.write(line)
 
@@ -47,7 +53,7 @@ def update_ip_in_env_infos(env_name, new_ip):
     if not os.path.exists(env_info_path):
         exit_with_error("找不到 env_infos.json 文件，请确保文件存在并包含正确的环境信息")
 
-    with open("env_infos.json", "r") as json_file:
+    with open(env_info_path, "r") as json_file:
         env_infos = json.load(json_file)
 
     if env_name not in env_infos:
@@ -55,7 +61,7 @@ def update_ip_in_env_infos(env_name, new_ip):
 
     env_infos[env_name]["public_ip"] = new_ip
 
-    with open("env_infos.json", "w") as json_file:
+    with open(env_info_path, "w") as json_file:
         json.dump(env_infos, json_file, indent=2)
 
     print(f"已更新环境 {env_name} 的 IP 为 {new_ip}")
@@ -64,7 +70,7 @@ def update_ip_in_env_infos(env_name, new_ip):
 def get_ip(env_name):
     if not os.path.exists(env_info_path):
         exit_with_error("找不到 env_infos.json 文件，请确保文件存在并包含正确的环境信息")
-    with open("env_infos.json", "r") as json_file:
+    with open(env_info_path, "r") as json_file:
         env_infos = json.load(json_file)
     if env_name not in env_infos or "public_ip" not in env_infos[env_name]:
         exit_with_error(f"找不到环境 {env_name} 的 IP，请手动输入")
@@ -77,7 +83,8 @@ def parse_command_line_args():
     # 添加位置参数
     parser.add_argument("env_name", help="Name of the environment")
     parser.add_argument("--ip", help="remote server ip", required=False)
-    parser.add_argument("--port", help="ssh port, default is 18443", required=False)
+    parser.add_argument("--port", help="ssh port, default is 8122", required=False, default="8122")
+    parser.add_argument("--only-restart", action="store_true", help="Only restart the service")
 
     args = parser.parse_args()
 
@@ -86,6 +93,8 @@ def parse_command_line_args():
 
 if __name__ == '__main__':
     try:
+        # 获取当前 shell 的环境变量
+        current_env = os.environ.copy()
         # 检查参数
         args = parse_command_line_args()
         # 如果未提供 IP，检查 env_infos.json 文件
@@ -98,39 +107,44 @@ if __name__ == '__main__':
             # TODO 先测试 IP 是否可用
             update_ip_in_env_infos(env_name, ip)
 
-        # 执行 scp 和 ssh 命令
-        subprocess.run(["scp", "-P", port, "bin/ones-ai-api-core.tar.gz", f"root@{ip}:/tmp/ones-ai-api-core.tar.gz"])
-        subprocess.run(["ssh", "-p", port, f"root@{ip}",
-                        "sh /tmp/debug_tool/debug.sh && docker exec -i $(docker ps -q --filter 'publish=80') /bin/bash -c 'tail -f /usr/local/ones-ai-project-api/nohup.out'"])
-        # 检查当前目录是否为项目根目录，有 go.mod 文件，或者有 vendor 目录
-        if not (os.path.isfile('go.mod') or os.path.isdir('vendor')):
-            exit_with_error("不是 bang-api 项目根目录，请先切换到项目根目录。")
+        if not args.only_restart:
+            # 执行 scp 和 ssh 命令
+            # 检查当前目录是否为项目根目录，有 go.mod 文件，或者有 vendor 目录
+            if not (os.path.isfile('go.mod') or os.path.isdir('vendor')):
+                exit_with_error("不是 bang-api 项目根目录，请先切换到项目根目录。")
 
-        comment_code()
+            comment_code()
 
-        # 编译 Go 项目
-        check_result = subprocess.run(
-            ['CGO_ENABLED=0', 'GOOS=linux', 'GOARCH=amd64', 'go', 'build', '-o', 'bin/ones-ai-api-core', '-gcflags',
-             'all=-N -l'])
+            # 编译 Go 项目
+            # env={'CGO_ENABLED': '0', 'GOOS': 'linux', 'GOARCH': 'amd64'}
+            current_env['CGO_ENABLED'] = '0'
+            current_env['GOOS'] = 'linux'
+            current_env['GOARCH'] = 'amd64'
+            check_result = subprocess.run(
+                ['go', 'build', '-o', 'bin/ones-ai-api-core', '-gcflags', 'all=-N -l'],
+                env=current_env
+            )
 
-        # 检查编译是否成功
+            # 检查编译是否成功
+            if check_result.returncode != 0:
+                exit_with_error("Go build failed.")
+            check_result = subprocess.run(['tar', '-czvf', 'bin/ones-ai-api-core.tar.gz', 'bin/ones-ai-api-core'])
+            if check_result.returncode != 0:
+                exit_with_error("Compression failed.")
+            print("Compression and packaging successful.")
+            md5_output = subprocess.getoutput('md5 bin/ones-ai-api-core')
+            print(md5_output)
+            # 输出压缩包绝对路径
+            print("二进制已经压缩放在:", os.path.realpath('bin/ones-ai-api-core.tar.gz'))
+            scp_command = f"scp -P {port} bin/ones-ai-api-core.tar.gz root@{ip}:/tmp/ones-ai-api-core.tar.gz"
+            print(scp_command)
+            check_result = subprocess.run(scp_command, shell=True)
+            if check_result.returncode != 0:
+                exit_with_error("Upload failed.")
+        check_result = subprocess.run(["ssh", "-p", port, f"root@{ip}",
+                                       "sh /tmp/debug_tool/debug.sh && docker exec -i $(docker ps -q --filter 'publish=80') /bin/bash -c 'tail -f /usr/local/ones-ai-project-api/nohup.out'"])
         if check_result.returncode != 0:
-            exit_with_error("Go build failed.")
-        check_result = subprocess.run(['tar', '-czvf', 'bin/ones-ai-api-core.tar.gz', 'bin/ones-ai-api-core'])
-        if check_result.returncode != 0:
-            exit_with_error("Compression failed.")
-        print("Compression and packaging successful.")
-        md5_output = subprocess.getoutput('md5 bin/ones-ai-api-core')
-        print(md5_output)
-        # 输出压缩包绝对路径
-        print("二进制已经压缩放在:", os.path.realpath('bin/ones-ai-api-core.tar.gz'))
-        check_result = subprocess.run(['scp', '-P', port, 'bin/ones-ai-api-core.tar.gz',
-                                       f"root@{ip}:/tmp/ones-ai-api-core.tar.gz"])
-        if check_result.returncode != 0:
-            exit_with_error("Upload failed.")
-        check_result = subprocess.run(['ssh', '-p', port, f"root@{ip}", "sh /tmp/debug_tool/debug.sh"])
-        if check_result.returncode != 0:
-            exit_with_error("Upload failed.")
+            exit_with_error("start debug failed.")
     except ScriptError as e:
         print(f"Error: {e}")
         sys.exit(1)
--- remote_debug/install.py
+++ remote_debug/install.py
@@ -1,4 +1,4 @@
-# curl -o install.py http://112.74.98.121:8080/debug_tool/install.py && python install.py
+#!/usr/bin/env python
 import argparse
 import os
 import subprocess
@@ -20,39 +20,40 @@ def download_debug_tool():
     if os.path.isfile(debug_tool_path):
         exit_with_error("debug_tool.tar.gz already exists. If you want to update, please remove the existing one.")
 
-    subprocess.run(["curl", "-o", debug_tool_path, debug_tool_url])
+    subprocess.call(["curl", "-o", debug_tool_path, debug_tool_url])
 
 
 def extract_debug_tool():
-    subprocess.run(["tar", "-zxvf", "/tmp/debug_tool.tar.gz"], cwd="/tmp")
+    subprocess.call(["tar", "-zxvf", "/tmp/debug_tool.tar.gz", "-C", "/tmp"])
 
 
 def replace_container():
-    subprocess.run(["sh", "/tmp/debug_tool/replace_container.sh"])
+    subprocess.call(["sh", "/tmp/debug_tool/replace_container.sh"])
 
 
-def configure_ssh(public_key):
+def configure_ssh(public_key, port):
     authorized_keys_path = os.path.expanduser("~/.ssh/authorized_keys")
 
     with open(authorized_keys_path, "a") as auth_keys_file:
         auth_keys_file.write(public_key + "\n")
 
-    subprocess.run(["sed", "-i", "-e", "/^#Port 22/a Port 18443", "-e", "s/^#Port 22/Port 22/", "/etc/ssh/sshd_config"])
+    sed_command = 'sed -i -e "/^#Port 22/a Port {0}" -e "s/^#Port 22/Port 22/" /etc/ssh/sshd_config'.format(port)
+    subprocess.call(sed_command, shell=True)
 
-    subprocess.run(["systemctl", "restart", "sshd"])
+    subprocess.call(["systemctl", "restart", "sshd"])
 
 
 def get_local_ip():
-    ip_address = subprocess.getoutput("curl -s ip.me").strip()
+    ip_address = subprocess.check_output(["curl", "-s", "ip.me"]).strip()
     return ip_address
 
 
 def parse_command_line_args():
     parser = argparse.ArgumentParser(description="install debug tool.")
-
     parser.add_argument("public_key", help="public key used for ssh")
+    parser.add_argument("--port", help="ssh listen port, default is 8122, your can use 9200, 9001, 9002, 8123",
+                        required=False, default="8122")
     args = parser.parse_args()
-
     return args
 
 
@@ -61,19 +62,21 @@ if __name__ == '__main__':
         os.chdir("/tmp")
         args = parse_command_line_args()
         public_key = args.public_key
+        port = args.port
 
         download_debug_tool()
         extract_debug_tool()
         replace_container()
-
-        configure_ssh(public_key)
+        configure_ssh(public_key, port)
 
         ip_address = get_local_ip()
 
-        print(f"The tool package has been downloaded to the /tmp/debug_tool directory. "
-              f"Please download the compile and package script locally and execute it.")
-        print(f"Download command: ")
-        print(f"curl -o build_deploy.py http://112.74.98.121:8080/build_deploy.py")
+        print("The tool package has been downloaded to the /tmp/debug_tool directory.")
+        print("Please download the compile and package script locally and execute it.")
+        print("Download command: ")
+        print("curl -o build_deploy.py http://112.74.98.121:8080/build_deploy.py")
+        print("Execute command: ")
+        print("python build_deploy.py replace_your_env_name --ip {} --port {}".format(ip_address, port))
     except ScriptError as e:
-        print(f"Error: {e}")
-    sys.exit(1)
+        print("Error: {0}".format(e))
+        sys.exit(1)
