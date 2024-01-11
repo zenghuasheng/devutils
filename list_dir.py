2f79b84ab794ab659f8050237357adde0e17419d
zenghuasheng <zenghuasheng@ones.ai>
2024-01-11 15:59:34 +0800

    add: 解耦相关的脚本都放在一个目录里

---
+++ list_dir.py
@@ -0,0 +1,19 @@
+import os
+
+if __name__ == '__main__':
+    main_directory = '/Users/xhs/go/src/github.com/bangwork/bang-api-gomod/app'
+    base = os.path.basename(main_directory)
+    dirs = []
+    for dirpath, dirnames, filenames in os.walk(main_directory):
+        dirs.append(dirpath)
+    # 替换掉 /Users/xhs/go/src/github.com/bangwork/bang-api-gomod/
+    relative_dirs = []
+    for item in dirs:
+        if item == main_directory:
+            continue
+        relative_dirs.append(item.replace(os.path.dirname(main_directory) + '/', ''))
+    # 按 / 分割，取 len = 2 的
+    for item in relative_dirs:
+        if len(item.split('/')) != 3:
+            continue
+        print(f'        "{item}": ("", ""),')
