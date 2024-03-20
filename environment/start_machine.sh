179463e7673fc4e1d57cee58ec521376014b0b8d
zenghuasheng <zenghuasheng@ones.ai>
2024-03-20 17:34:44 +0800

    add: 开机

---
+++ environment/start_machine.sh
@@ -0,0 +1,18 @@
+curl 'https://devopsour.ones.pro/api/environment/start' \
+-X POST \
+-H 'Host: devopsour.ones.pro' \
+-H 'Connection: keep-alive' \
+-H 'sec-ch-ua: "Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"' \
+-H 'Accept: application/json, text/plain, */*' \
+-H 'sec-ch-ua-mobile: ?0' \
+-H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36' \
+-H 'sec-ch-ua-platform: "macOS"' \
+-H 'Origin: https://devopsour.ones.pro' \
+-H 'Sec-Fetch-Site: same-origin' \
+-H 'Sec-Fetch-Mode: cors' \
+-H 'Sec-Fetch-Dest: empty' \
+-H 'Referer: https://devopsour.ones.pro/' \
+-H 'Accept-Language: zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7' \
+-H 'Content-Type: application/json' \
+--cookie 'sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%22R9o8QgKu%22%2C%22first_id%22%3A%22181ae269bf5cf6-02d82d82d82d82e-37796305-1296000-181ae269bf61a2e%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E7%9B%B4%E6%8E%A5%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC_%E7%9B%B4%E6%8E%A5%E6%89%93%E5%BC%80%22%2C%22%24latest_referrer%22%3A%22%22%7D%2C%22identities%22%3A%22eyIkaWRlbnRpdHlfbG9naW5faWQiOiJSOW84UWdLdSIsIiRpZGVudGl0eV9jb29raWVfaWQiOiIxODFhZTI2OWJmNWNmNi0wMmQ4MmQ4MmQ4MmQ4MmUtMzc3OTYzMDUtMTI5NjAwMC0xODFhZTI2OWJmNjFhMmUifQ%3D%3D%22%2C%22history_login_id%22%3A%7B%22name%22%3A%22%24identity_login_id%22%2C%22value%22%3A%22R9o8QgKu%22%7D%2C%22%24device_id%22%3A%22181ae269bf5cf6-02d82d82d82d82e-37796305-1296000-181ae269bf61a2e%22%7D; our-devops-token=R9o8QgKu%3ABrKFa7nsNIUolpM7wEm7Pn1rdvCHprQlpASbCcTWtKwGU8bigjo1p4Gy79St1WGf' \
+--data-raw '{"id":140}'
